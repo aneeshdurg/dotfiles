e206775f189fec049b8c4dc084e5d4a75d9c61a5
Aneesh Durg <durg2@illinois.edu>
2018-03-11 16:35:16 -0500

    removed non-printable

--- wu-tang.sh
+++ wu-tang.sh
@@ -1,3 +1,3 @@
 name=$@
 name=$(echo $name | tr \  +)
-curl -i -X POST -H "Content-Type:  application/x-www-form-urlencoded" -d 'realname='$name'&Submit=Enter+the+Wu-Tang' http://www.mess.be/inickgenwuname.php 2>/dev/null | grep -A1 From | tr -d '\n' | rev | cut -d . -f1 | rev | cut -d \  -f2-
+curl -i -X POST -H "Content-Type:  application/x-www-form-urlencoded" -d 'realname='$name'&Submit=Enter+the+Wu-Tang' http://www.mess.be/inickgenwuname.php 2>/dev/null | tr -cd '\11\12\15\40-\176' | grep -A1 From | tr -d '\n' | rev | cut -d . -f1 | rev | cut -d \  -f2-
