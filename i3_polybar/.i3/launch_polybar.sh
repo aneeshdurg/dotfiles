bea65c0c8ef2a3625fc643bf244369af6e19153d
Aneesh Durg <durg2@illinois.edu>
2019-03-31 12:26:16 -0500

    Multi display interface

--- i3_polybar/.i3/launch_polybar.sh
+++ i3_polybar/.i3/launch_polybar.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 pkill polybar
+config=primary
 if type "xrandr"; then
+  readarray -t mirrors < <(xrandr --query | grep " connected" | grep -Eo '[0-9]+x[0-9]+\+[0-9]+\+[0-9]+')
+  if (( ${#mirrors[@]} == 2 )) && [[ "${mirrors[0]}" != "" && "${mirrors[0]}" == "${mirrors[1]}" ]]; then
+    config=mirrored
+  fi
+
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload $config &
   done
 else
-  polybar --reload example &
+  polybar --reload $config &
 fi
