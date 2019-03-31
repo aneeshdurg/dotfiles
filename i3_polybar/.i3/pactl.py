b23262bc6bdd68107d7968783107b3372754a934
Aneesh Durg <durg2@illinois.edu>
2019-03-31 14:18:41 -0500

    Added volume change sound

--- i3_polybar/.i3/pactl.py
+++ i3_polybar/.i3/pactl.py
@@ -3,6 +3,9 @@
 
 from sys import argv
 import pulsectl
+import subprocess
+
+BELL_SOUND = "/usr/share/sounds/freedesktop/stereo/audio-volume-change.oga"
 
 increment = 0
 if argv[1] == "up":
@@ -15,17 +18,22 @@ elif argv[1] != "mute":
 with pulsectl.Pulse('volume-increaser') as pulse:
     for sink in pulse.sink_list():
         if increment:
+            delta = increment
             vol = max(sink.volume.values)
             if vol >= 1 and increment > 0:
-                exit(1)
+                delta = 0
             elif vol <= 0 and increment < 0:
-                exit(1)
+                delta = 0
             offset = (vol  % 0.05)
             tol = 0.0001
             if (offset > tol and offset < (0.05 - tol)):
                 pulse.volume_change_all_chans(sink, -1 * (vol  % 0.05))
-            pulse.volume_change_all_chans(sink, increment)
+            pulse.volume_change_all_chans(sink, delta)
             if (sink.mute == 1):
                 pulse.mute(sink, mute=False)
+
         else:
             pulse.mute(sink, mute=sink.mute==0)
+
+if increment:
+    subprocess.check_call(["paplay", BELL_SOUND])
