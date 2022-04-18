664b733f64c6bef555e89cd84486032fa5a9d51f
Aneesh Durg <aneeshdurg17@gmail.com>
2022-04-18 10:30:47 -0500

    Delete wu-tang.sh

--- wu-tang.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-name=$@
-name=$(echo $name | tr \  +)
-curl -i -X POST -H "Content-Type:  application/x-www-form-urlencoded" -d 'realname='$name'&Submit=Enter+the+Wu-Tang' http://www.mess.be/inickgenwuname.php 2>/dev/null | tr -cd '\11\12\15\40-\176' | grep -A1 From | tr -d '\n' | rev | cut -d . -f1 | rev | cut -d \  -f2-
